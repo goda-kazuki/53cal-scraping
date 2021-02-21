395fc0065e1133b7c7719c3d278444400cb532d2
goda-kazuki <goda@eyemovic.com>
2021-02-21 17:02:46 +0900

    情報が取得できるようになったところまで

--- opt/index.py
+++ opt/index.py
@@ -1,10 +1,18 @@
 import requests
-from bs4 import BeautifulSoup
-# Webページを取得して解析する
+from bs4 import BeautifulSoup as bs4
+import re
 
-load_url = "https://www.ymori.com/books/python2nen/test1.html"
+load_url = "http://www.53cal.jp/areacalendar/?city=1380101&area=1380101435&yy=2021&mm=2"
 html = requests.get(load_url)
-soup = BeautifulSoup(html.content, "html.parser")
+soup = bs4(html.content, "html.parser")
 
-# HTML全体を表示する
-print(soup)
+# ゴミ出しの対象となっている日のリスト
+theDays = soup(class_="theday")
+
+for theDay in theDays:
+    imgTagSrc = theDay.find('img').get('src')
+    day = re.search(r'[0-9]+', imgTagSrc).group()
+
+
+    title = theDay.find('a').get('title')
+    print(day + title)
